44a59a38f8d419852924a9981eb4ca3b6c2d1430
Pawel Worzala <pawelworzala86@gmail.com>
2025-10-14 20:45:22 +0200

    refaktoring

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const app = express()
 
-app.use(express.static(path.resolve('./public')))
+app.use(express.static(path.resolve('./dist')))
 
 /*app.get('/', (req, res) => {
   res.send('Hello World')
